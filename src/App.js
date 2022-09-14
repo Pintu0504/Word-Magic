8753a328b441d9e42916cd664d4eef765e8235d0
Pintu Kumar <pin205kmr@gmail.com>
2022-09-15 01:47:15 +0530

    Dark theme+clear button added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import {useState} from 'react'
 
 function App() {
+  const [mode, setMode] = useState("light")
+  const toggleMode = ()=>{
+    if(mode === 'dark') {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    } else {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" about="About" />
-      <TextForm heading="Enter your text below" />
+      <Navbar title="Word Magic" mode={mode} toggleMode={toggleMode}/>
+      <TextForm heading="Enter your text below" mode={mode} />
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,7 +13,7 @@ const WordCharCount = ({text}) => {
     )
 }
 
-export default function TextForm({heading}) {
+export default function TextForm({heading, mode}) {
     const [text, setText] = useState("")
 
     const handleOnChange = (e)=>{
@@ -29,25 +29,31 @@ export default function TextForm({heading}) {
         const newText = text.toLowerCase()
         setText(newText)
     }
+    
+    const clearText = ()=>{
+        const newText = "";
+        setText(newText)
+    }
 
     return (
         <>
-            <div className="container">
+            <div className="container" style={{color: mode === 'dark' ? 'white' : 'black'}}>
                 <h1>{heading}</h1>
                 <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{color: mode === 'dark' ? 'white' : 'black', backgroundColor: mode === 'dark' ? 'grey' : 'white'}}></textarea>
                 </div>
                 <button className="btn btn-primary" onClick={changeUpperCase}>Convert Uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={changeLowerCase}>Convert Lowercase</button>
+                <button className="btn btn-primary" onClick={clearText}>Clear</button>
             </div>
 
-            <div className='container my-2'>
+            <div className='container my-2' style={{color: mode === 'dark' ? 'white' : 'black'}}>
                 <h3 className="">Your Text Summary</h3>
                 <WordCharCount text={text}/>
                 <p>{0.007 * text.split(' ').length} Minutes to read</p>
 
                 <h3>Preview</h3>
-                {text}
+                {text.length === 0 ? "Nothing to show preview" : text}
             </div>
         </> 
     )
